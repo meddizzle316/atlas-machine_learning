bc2edc69759239828d31adcb2242d59292e536d6
meddizzle316 <meddizzle316@gmail.com>
2024-04-29 12:57:41 -0500

    first go at cbrain 2

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+
+    result = []
+    first: int = 0
+    second: int = 0
+    third: int = 0
+    for i, dimension in enumerate(matrix):
+        first += 1
+        for x, array in enumerate(dimension):
+            if x > second:
+                second = x
+            try:
+                for y, list in enumerate(array):
+                    if y > third:
+                        third = y
+            except TypeError:
+                pass
+
+    result.append(first)
+    if second > 0:
+        result.append(second + 1)
+    if third > 0:
+        result.append(third + 1)
+    return result
