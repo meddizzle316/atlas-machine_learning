2045d76194d55d19b4bffe92f3b38ace661c7ef3
meddizzle316 <meddizzle316@gmail.com>
2024-04-29 16:58:27 -0500

    trying something out

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python3
 
-# def matrix_dimensions(matrix):
-#     if isinstance(matrix, list):
-#         if matrix and isinstance(matrix[0], list):
-#             return 1 + matrix_dimensions(matrix[0])
-#         else:
-#             return 1
-#     else:
-#         return 0
+def matrix_dimensions(matrix):
+    if isinstance(matrix, list):
+        if matrix and isinstance(matrix[0], list):
+            return 1 + matrix_dimensions(matrix[0])
+        else:
+            return 1
+    else:
+        return 0
+    
+def matrix_length(matrix, x, n):
+    if isinstance(matrix, list):
+        if matrix and isinstance(matrix[0], list) and x < n:
+            x += 1
+            return matrix_length(matrix[0], x, n)
+        elif matrix and x == n: 
+            return len(matrix)
+    else:
+        return 0
 
 def matrix_shape(matrix):
     result = []
-    # number_of_dimensions = matrix_dimensions(matrix)
-    for i in matrix:
-        result.append(len(matrix))
+    number_of_dimensions = matrix_dimensions(matrix)
+    # print(number_of_dimensions)
+    i: int = 0
+    while i < number_of_dimensions:
+        try:
+            result.append(matrix_length(matrix, 0, i))
+        except TypeError:
+            pass
+        i += 1
+    # length_of_first = matrix_length(matrix, 0, 0)
+    # print(length_of_first)
+    # length_of_second = matrix_length(matrix, 0, 1)
+    # print(length_of_second)
+    # length_of_third = matrix_length(matrix, 0, 2)
+    # print(length_of_third)
+    # for i in matrix:
+    #     result.append(len(matrix))
     return result
 
 
