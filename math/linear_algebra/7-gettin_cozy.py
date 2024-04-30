429344d0451eb5178ca48d2aafe1100bc6fe1e56
meddizzle316 <meddizzle316@gmail.com>
2024-04-30 14:22:36 -0500

    trying another thing

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -44,8 +44,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
     new_matrix = []
     # if matrix_shape(mat1)[0] != matrix_shape(mat2)[0]:
     #     return None
-    if len(mat1[0]) == 0 or len(mat2[0]) == 0:
-        return None
+    # if len(mat1[0]) == 0 or len(mat2[0]) == 0:
+    #     return None
     try:
         if len(mat1[1]) != len(mat2[0]):
             return None
