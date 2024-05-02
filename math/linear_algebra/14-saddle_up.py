776552415f8099076e9e39da57df9dab2fb01176
meddizzle316 <meddizzle316@gmail.com>
2024-05-02 17:16:48 -0500

    14 is passing locally

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module that performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """module that performs matrix multiplication"""
+    return np.dot(mat1, mat2)
