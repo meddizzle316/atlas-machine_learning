6ca717492b9bee146fffe225871002eb5ae6231a
meddizzle316 <meddizzle316@gmail.com>
2024-05-02 11:56:25 -0500

    new null check

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -44,9 +44,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """couldn't get dimension or empty matrix check"""
     new_matrix = []
 
-    if axis == 0 and len(mat1) != len(mat2):
+    if axis == 0 and len(mat1[0]) != len(mat2[0]):
         return None
-    elif axis == 1 and len(mat1[0]) != len(mat2[0]):
+    elif axis == 1 and len(mat1) != len(mat2):
         return None
     if axis == 0:
         new_matrix = mat1 + mat2
