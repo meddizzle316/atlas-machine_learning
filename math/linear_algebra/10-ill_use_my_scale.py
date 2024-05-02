f9c28dea37feb224aba249e9cc8de83dd03bd2c0
meddizzle316 <meddizzle316@gmail.com>
2024-05-02 16:57:13 -0500

    adding 10 and 11

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+"""function that returns the shape of a matrix"""
+
+
+def np_shape(matrix):
+    """function that returns the shape of a matrix"""
+    nparray = np.array(matrix)
+    return nparray.shape
