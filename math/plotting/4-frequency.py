b9c1377e234c2b07b421ccfd402d0b94a8bfe43a
meddizzle316 <meddizzle316@gmail.com>
2024-05-07 10:54:16 -0500

    pyfix

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""plots grades"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def frequency():
 
+def frequency():
+    """plots grades"""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
