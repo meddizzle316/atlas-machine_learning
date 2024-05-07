bd04fd152e3668b797d9d6da022fb8ac9c82f861
meddizzle316 <meddizzle316@gmail.com>
2024-05-07 10:47:24 -0500

    pyfix

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""plots a line"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
 
+def line():
+    """plots a line"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
