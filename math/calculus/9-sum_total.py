361c0d6bb4201418cbe0695018a8c76665452581
meddizzle316 <meddizzle316@gmail.com>
2024-05-08 10:38:55 -0500

    null case

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,5 +5,7 @@ import numpy as np
 
 def summation_i_squared(n):
     """squares and adds"""
+    if not isinstance(n, int):
+        return None
     list = np.linspace(1, n, n)
     return (int(sum(list ** 2)))
