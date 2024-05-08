d399e39182147682e9a928024dcf7f2339c529ab
meddizzle316 <meddizzle316@gmail.com>
2024-05-08 10:25:42 -0500

    base case

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,7 +6,5 @@ def summation_i_squared(n):
     """squares and adds"""
     if n == 1:
         return 1
-    elif isinstance(n, int):
-        return (n ** 2) + summation_i_squared(n-1)
     else:
-        return None
+        return (n ** 2) + summation_i_squared(n-1)
