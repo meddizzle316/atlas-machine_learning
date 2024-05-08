21ee0f046e8d8cb61e428f4e8023cb666e825582
meddizzle316 <meddizzle316@gmail.com>
2024-05-08 11:08:35 -0500

    new null case

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,11 +6,14 @@ def poly_derivative(poly):
     """gets derivative of given poly"""
     new_list = []
     i = 1
-    all_int = all(isinstance(item, (int)) for item in poly)
-    if len(poly) < 1 or not all_int:
+    if not isinstance(poly, list):
         return None
     if len(poly) == 1:
         return [0]
+    all_int = all(isinstance(item, (int)) for item in poly)
+    if len(poly) < 1 or not all_int:
+        return None
+
     while i < len(poly):
         new_list.append(poly[i] * i)
         i += 1
