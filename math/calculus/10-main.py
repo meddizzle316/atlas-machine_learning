f07c918c00f7c7fc23f69c1d95239239b4992315
meddizzle316 <meddizzle316@gmail.com>
2024-05-08 11:02:34 -0500

    trying new null case

--- math/calculus/10-main.py
+++ math/calculus/10-main.py
@@ -2,5 +2,5 @@
 
 poly_derivative = __import__('10-matisse').poly_derivative
 
-poly = [5, 3, 0, 1]
+poly = [5, 3, 0, 1, 'three']
 print(poly_derivative(poly))
--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,6 +6,9 @@ def poly_derivative(poly):
     """gets derivative of given poly"""
     new_list = []
     i = 1
+    all_int = all(isinstance(item, (int, float)) for item in poly)
+    if len(poly) < 1 or not all_int:
+        return None
     while i < len(poly):
         new_list.append(poly[i] * i)
         i += 1
