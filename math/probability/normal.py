2dddf95f8432f88fd5d19afc2f38d58982ed2f25
meddizzle316 <meddizzle316@gmail.com>
2024-05-13 15:38:28 -0500

    trying the standard deviation of normal

---
+++ math/probability/normal.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""class for normal distribution"""
+
+
+class Normal:
+    """class for normal distribution"""
+    def __init__(self, data=None, mean=0, stddev=1.):
+        """init func"""
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = float((sum(data) / len(data)))
+            self.stddev = 0
+            temp = 0
+            for i in data:
+               temp += (i - self.mean) ** 2
+            temp = temp / (len(data))
+            temp = temp ** 0.5
+            self.stddev = temp
+        else:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
