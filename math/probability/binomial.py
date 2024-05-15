36b92c47664e3097e87b17cecd8ae42286b43033
meddizzle316 <meddizzle316@gmail.com>
2024-05-14 21:33:39 -0500

    trying but without recursion this time

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -44,10 +44,10 @@ class Binomial:
 
     def f(self, n):
         """gets the factorial of a number"""
-        if n == 0 or n == 1:
-            return 1
-        else:
-            return n * self.f(n - 1)
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
 
     def pmf(self, k):
         """probability mass function of binomial"""
