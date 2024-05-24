65e9106d6e1aebc57910675e66780e8ef0cc6fa7
meddizzle316 <meddizzle316@gmail.com>
2024-05-24 11:43:04 -0500

    seeing about weird edge case

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -2,13 +2,14 @@
 """class for neuron"""
 import numpy as np
 
+
 class Neuron:
     """class for neuron"""
     def __init__(self, nx):
         """init"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        if nx < 1:
+        if nx <= 1:
             raise ValueError("nx must be a positive integer")
         self.W = np.random.normal(np.zeros((1, 784)))
         self.b = 0
