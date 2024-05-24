9b968b5a9ad300b67291a07ae7966972cbf7aff7
meddizzle316 <meddizzle316@gmail.com>
2024-05-24 11:40:37 -0500

    trying out the first one

--- supervised_learning/classification/0-main.py
+++ supervised_learning/classification/0-main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
-import matplotlib.pyplot as plt
 import numpy as np
+
+Neuron = __import__('0-neuron').Neuron
 
 lib_train = np.load('../data/Binary_Train.npz')
 X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
 
-fig = plt.figure(figsize=(10, 10))
-for i in range(100):
-    fig.add_subplot(10, 10, i + 1)
-    plt.imshow(X_3D[i])
-    plt.title(Y[0, i])
-    plt.axis('off')
-plt.tight_layout()
-plt.show()
+np.random.seed(0)
+neuron = Neuron(X.shape[0])
+print(neuron.W)
+print(neuron.W.shape)
+print(neuron.b)
+print(neuron.A)
+neuron.A = 10
+print(neuron.A)
---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""class for neuron"""
+import numpy as np
+
+class Neuron:
+    """class for neuron"""
+    def __init__(self, nx):
+        """init"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.normal(np.zeros((1, 784)))
+        self.b = 0
+        self.A = 0
