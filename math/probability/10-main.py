2a12967b24751124f2f406bc7390636af8fa0ec1
meddizzle316 <meddizzle316@gmail.com>
2024-05-24 11:14:08 -0500

    ljkashjdfjklasdf

--- math/probability/10-main.py
+++ math/probability/10-main.py
@@ -4,7 +4,8 @@ import numpy as np
 Binomial = __import__('binomial').Binomial
 
 np.random.seed(0)
-data = np.random.binomial(50, 0.6, 100).tolist()
+data = np.random.binomial(50, 0.5, 100).tolist()
+print(data)
 b1 = Binomial(data)
 
 print('n:', b1.n, "p:", b1.p)
