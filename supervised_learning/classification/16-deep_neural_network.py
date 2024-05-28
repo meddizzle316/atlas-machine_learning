18de620d1ba9d915a349b42c6545e1f908f23818
meddizzle316 <meddizzle316@gmail.com>
2024-05-28 14:46:35 -0500

    pyfix

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -5,30 +5,32 @@ import numpy as np
 
 class DeepNeuralNetwork():
     """class for deepneuralnetwork"""
-    def __init__(self, nx, layers):
+    def __init__(self, nx, lay):
         """init func"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if not isinstance(layers, list):
-            raise TypeError("layers must be a list of positive integers")
+        if not isinstance(lay, list):
+            raise TypeError("lay must be a list of positive integers")
 
-        test_array = np.array([layers])
-        if not np.all(test_array > 0) or len(layers) == 0:
-            raise TypeError("layers must be a list of positive integers")
+        test_array = np.array([lay])
+        if not np.all(test_array > 0) or len(lay) == 0:
+            raise TypeError("lay must be a list of positive integers")
 
-        self.L = len(layers)
+        self.L = len(lay)
         self.cache = {}
         self.weights = {}
 
         # setting dimensions and initialization of first layer
-        self.weights[f"W{1}"] = np.random.randn(layers[0], nx) * np.sqrt(2/nx)
-        self.weights[f"b{1}"] = np.zeros((layers[0], 1))
+        self.weights[f"W{1}"] = np.random.randn(lay[0], nx) * np.sqrt(2/nx)
+        self.weights[f"b{1}"] = np.zeros((lay[0], 1))
 
-        # setting dimensions and initialization of 2nd to the last layers
+        # setting dimensions and initialization of 2nd to the last lay
         for i in range(1, self.L):
             # initialize using He et al method (mostly the sqrt at the end?)
-            self.weights[f"W{i + 1}"] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2./layers[i - 1])
-            self.weights[f"b{i + 1}"] = np.zeros((layers[i], 1))
+            # X: dumb pycode format fix for line 35
+            X = np.sqrt(2./lay[i - 1])
+            self.weights[f"W{i + 1}"] = np.random.randn(lay[i], lay[i - 1]) * X
+            self.weights[f"b{i + 1}"] = np.zeros((lay[i], 1))
