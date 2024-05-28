1d9a5b783c0bd23772a38d8ac3ebd16b4830f9c4
meddizzle316 <meddizzle316@gmail.com>
2024-05-28 14:39:37 -0500

    pyfix?

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class DeepNeuralNetwork():
     """class for deepneuralnetwork"""
     def __init__(self, nx, layers):
@@ -14,21 +15,21 @@ class DeepNeuralNetwork():
 
         if not isinstance(layers, list):
             raise TypeError("layers must be a list of positive integers")
-        
+
         test_array = np.array([layers])
-        if not np.all(test_array > 0):    
+        if not np.all(test_array > 0):
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        
+
         # setting dimensions and initialization of first layer
         self.weights[f"W{1}"] = np.random.randn(layers[0], nx) * np.sqrt(2/nx)
         self.weights[f"b{1}"] = np.zeros((layers[0], 1))
 
         # setting dimensions and initialization of 2nd to the last layers
-        for index in range(1, self.L):
+        for i in range(1, self.L):
             # initialize using He et al method (mostly the sqrt at the end?)
-            self.weights[f"W{index + 1}"] = np.random.randn(layers[index], layers[index-1]) * np.sqrt(2./layers[index -1])
-            self.weights[f"b{index + 1}"] = np.zeros((layers[index], 1))
+            self.weights[f"W{i + 1}"] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2./layers[i - 1])
+            self.weights[f"b{i + 1}"] = np.zeros((layers[i], 1))
