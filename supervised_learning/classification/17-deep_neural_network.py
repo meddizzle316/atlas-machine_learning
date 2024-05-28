d1bde0162b70808870d119ff99a652bb5543873e
meddizzle316 <meddizzle316@gmail.com>
2024-05-28 14:55:33 -0500

    trying 17

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""defines a deep neural network"""
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """class for deepneuralnetwork"""
+    def __init__(self, nx, lay):
+        """init func"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(lay, list):
+            raise TypeError("layers must be a list of positive integers")
+
+        test_array = np.array([lay])
+        if not np.all(test_array > 0) or len(lay) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(lay)
+        self.__cache = {}
+        self.__weights = {}
+
+        # setting dimensions and initialization of first layer
+        self.__weights[f"W{1}"] = np.random.randn(lay[0], nx) * np.sqrt(2 / nx)
+        self.__weights[f"b{1}"] = np.zeros((lay[0], 1))
+
+        # setting dimensions and initialization of 2nd to the last lay
+        for i in range(1, self.L):
+            # initialize using He et al method (mostly the sqrt at the end?)
+            # X and rand: dumb pycode format fix line 35
+            X = np.sqrt(2. / lay[i - 1])
+            rand = np.random.randn(lay[i], lay[i - 1])
+            self.__weights[f"W{i + 1}"] = rand * X
+            self.__weights[f"b{i + 1}"] = np.zeros((lay[i], 1))
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
