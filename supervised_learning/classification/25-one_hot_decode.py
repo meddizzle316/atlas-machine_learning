2124d4a0bbd80177283e7067cd5959433652be91
meddizzle316 <meddizzle316@gmail.com>
2024-05-29 16:20:10 -0500

    trying something

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -5,6 +5,10 @@ def one_hot_decode(one_hot):
     """decodes one hot"""
     import numpy as np
     
+    if type(one_hot) is not np.ndarray:
+        return None
+    if len(one_hot) < 2:
+        return None
     decoded_data = []
     decoded_data = np.argmax(one_hot, axis=0)
     # decoded_data = np.append(decoded_data, decoded_data[0])
