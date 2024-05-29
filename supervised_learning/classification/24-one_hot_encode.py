5df8042daa97897fdb1bd87121bf054c8dadd784
meddizzle316 <meddizzle316@gmail.com>
2024-05-29 12:41:44 -0500

    trying this

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -4,6 +4,10 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """encoding function"""
+    if type(Y) is not np.ndarray or not isinstance(classes, int):
+        return None
+    if classes < 2 or classes < np.amax(Y):
+        return None
     encoded_data = np.zeros((len(Y), classes))
 
     encoded_data[Y, np.arange(Y.size)] = 1
