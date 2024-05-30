9e7c342682d7ac59a9ac708e72f329056b24e530
meddizzle316 <meddizzle316@gmail.com>
2024-05-30 09:58:57 -0500

    retrying 25

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -12,7 +12,7 @@ def one_hot_decode(one_hot):
     if one_hot.ndim != 2:
         return None
     decoded_data = []
-    decoded_data = np.argmax(one_hot, axis=0)
+    decoded_data = np.argmax(one_hot, axis=1)
     # decoded_data = np.append(decoded_data, decoded_data[0])
     # decoded_data = np.delete(decoded_data, 0)
     return decoded_data
