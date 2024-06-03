c12358dd1f42d60e6f3733a9716fdab0fd1323c1
meddizzle316 <meddizzle316@gmail.com>
2024-06-03 14:01:36 -0500

    trying out 2

---
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""for stupid stupid tf1"""
+import tensorflow.compat.v1 as tf
+tf.compat.v1.disable_eager_execution()
+create_layer = __import__('1-create_layer').create_layer
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """does forward prop"""
+
+    current_layer = x
+    for i in range(len(layer_sizes)):
+        num_nodes = layer_sizes[i]
+        activation_func = activations[i]
+
+        current_layer = create_layer(current_layer, num_nodes, activation_func)
+
+    return current_layer
