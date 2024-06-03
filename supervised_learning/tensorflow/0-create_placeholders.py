9f23a900e4de050c99b9461063d67c476c1acae6
meddizzle316 <meddizzle316@gmail.com>
2024-06-03 13:43:03 -0500

    trying this out

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""stupid tf1 thing even though tf2 is a thing"""
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+
+
+def create_placeholders(nx, classes):
+    """returns two place holders"""
+    x = tf.compat.v1.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.compat.v1.placeholder(tf.float32, shape=[None, classes], name='y')
+
+    return x, y
