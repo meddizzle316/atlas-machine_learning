05ad777ee6a5b3f71a8e4049d5c5d730dc8d6c66
meddizzle316 <meddizzle316@gmail.com>
2024-06-04 09:19:23 -0500

    trying this

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -6,6 +6,10 @@ tf.compat.v1.disable_eager_execution()
 
 def calculate_accuracy(y, y_pred):
     """predicts accuracy"""
-    result = tf.math.divide(y, y_pred)
-    named_result = tf.reshape(result, [-1], name='Mean')
+    # result = tf.math.divide(y, y_pred)
+    # named_result = tf.reshape(result, [-1], name='Mean')
+    
+    percent_correct = tf.equal(tf.argmax(y_pred, axis=-1), tf.argmax(y, axis=-1))
+    
+    named_result = tf.reduce_mean(tf.cast(percent_correct, tf.float32))
     return named_result
