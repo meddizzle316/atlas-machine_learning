4322ed695201b8b0c125c6289f123cd41f1ffb0f
meddizzle316 <meddizzle316@gmail.com>
2024-06-07 16:45:57 -0500

    trying this out

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -6,10 +6,12 @@ import numpy as np
 def shuffle_data(X, Y):
     """shuffles data points in two matrices the same way"""
 
-    shuffled_X = np.empty(X.shape)
-    shuffled_Y = np.empty(Y.shape)
-    permutation = np.random.permutation(len(X))
-    for old_index, new_index in enumerate(permutation):
-        shuffled_X[new_index] = X[old_index]
-        shuffled_Y[new_index] = Y[old_index]
-    return shuffled_X, shuffled_Y
+    # shuffled_X = np.empty(X.shape)
+    # shuffled_Y = np.empty(Y.shape)
+    # permutation = np.random.permutation(len(X))
+    # for old_index, new_index in enumerate(permutation):
+    #     shuffled_X[new_index] = X[old_index]
+    #     shuffled_Y[new_index] = Y[old_index]
+    # return shuffled_X, shuffled_Y
+    p = np.random.permutation(len(X))
+    return X[p], Y[p]
