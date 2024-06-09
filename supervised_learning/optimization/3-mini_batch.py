47213eb5a107255c1e4f57952d9940cfcf04b437
meddizzle316 <meddizzle316@gmail.com>
2024-06-09 14:36:38 -0500

    pyfix

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -7,8 +7,9 @@ shuffle_data = __import__('2-shuffle_data').shuffle_data
 def create_mini_batches(X, Y, batch_size):
     """creates mini-batches, doesn't train"""
     indices = np.arange(X.shape[0])
+    print(f"Indices {indices}")
     np.random.shuffle(indices)
+    print(f"After shuffle {indices}")
     for start_index in range(0, X.shape[0] - batch_size + 1, batch_size):
         excerpt = indices[start_index:start_index + batch_size]
         yield X[excerpt], Y[excerpt]
-
