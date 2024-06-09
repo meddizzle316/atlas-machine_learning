a694e91b912eb893c4dfb22bf88e5fc3e61cf074
meddizzle316 <meddizzle316@gmail.com>
2024-06-09 14:41:48 -0500

    did some testing, now I more or less understand

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -7,9 +7,10 @@ shuffle_data = __import__('2-shuffle_data').shuffle_data
 def create_mini_batches(X, Y, batch_size):
     """creates mini-batches, doesn't train"""
     indices = np.arange(X.shape[0])
-    print(f"Indices {indices}")
     np.random.shuffle(indices)
-    print(f"After shuffle {indices}")
     for start_index in range(0, X.shape[0] - batch_size + 1, batch_size):
         excerpt = indices[start_index:start_index + batch_size]
+        # outputs numbers like [11774  3876 28702    66 18965  5006 37642 17812  1403 37740 27519  4449
+        #   6649 46204  6537 39997  7660 34661 21377 39712 10973 47200 41241 26995
+        #   7543 21504 34858 29257 12941 43542   651  3361]
         yield X[excerpt], Y[excerpt]
