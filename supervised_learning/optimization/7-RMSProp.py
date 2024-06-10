9dbbed163286363cef4a4c0dc31c191bf104d7c8
meddizzle316 <meddizzle316@gmail.com>
2024-06-10 11:59:27 -0500

    pyfxi

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -6,5 +6,5 @@ import numpy as np
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """updates a given variable using RMSProp"""
     Sderiv = (beta2 * s) + ((1 - beta2) * (grad ** 2))
-    mod_var = var - (alpha * (grad / (((Sderiv + epsilon)** 0.5)) ))
+    mod_var = var - (alpha * (grad / (((Sderiv + epsilon) ** 0.5))))
     return mod_var, Sderiv
