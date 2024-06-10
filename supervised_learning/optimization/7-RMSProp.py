de7d0f3d1cc822fa8adb91a71450d3d21aaf8911
meddizzle316 <meddizzle316@gmail.com>
2024-06-10 11:47:54 -0500

    pushing number 7

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""module for RMSProp from scratch"""
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """updates a given variable using RMSProp"""
+    Sderiv = (beta2 * s) + ((1 - beta2) * (grad ** 2))
+    mod_var = var - (alpha * (grad / (((Sderiv + epsilon)** 0.5)) ))
+    return mod_var, Sderiv
