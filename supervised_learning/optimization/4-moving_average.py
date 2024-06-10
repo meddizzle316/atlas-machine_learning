6d37d76cc4dbb623e31dee5374478e03365a3151
meddizzle316 <meddizzle316@gmail.com>
2024-06-10 10:17:14 -0500

    trying again

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -10,14 +10,11 @@ def moving_average(data, beta):
 
     moving_average = []
     for i in range(len(data)):
-        # print(f"data {data[i]}")
         if i > 0:
-            # print(f"moving_average[i-1]: {moving_average[i-1]}")
             v = (beta*moving_average[i-1]) + ((1 - beta) * data[i])
         else:
             v = (beta*0) + ((1 - beta) * data[i])
             v = v / (1 - np.power(beta, i + 1))
-        # print(f"this is v {v} for index {i}")
         moving_average.append(v)
     # moving_average.pop(0)
     return moving_average
