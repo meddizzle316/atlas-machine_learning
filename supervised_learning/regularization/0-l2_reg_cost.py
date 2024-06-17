8a798a1f0f55ce3f1a9a380e7359e12611e22f54
meddizzle316 <meddizzle316@gmail.com>
2024-06-17 12:07:12 -0500

    first go at 0

---
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""a module for l2 regularization cost"""
+import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """l2 reg cost function"""
+    w2 = 0
+    for i in range(1, L + 1):
+        weight_layer = weights[f"W{i}"]
+        w2 += np.sum(np.square(weight_layer))
+    second_part = (lambtha / (2 * m)) * (w2)
+    return cost + second_part
