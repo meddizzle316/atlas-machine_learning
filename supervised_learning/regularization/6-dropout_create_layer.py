2c632d9afd6a0e5d7458fe563f07cf43e5299e59
meddizzle316 <meddizzle316@gmail.com>
2024-06-20 11:11:26 -0500

    pyfxi

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -3,8 +3,11 @@
 import tensorflow as tf
 
 
-def dropout_create_layer(prev, n, activation, keep_prob,training=True):
+def dropout_create_layer(prev, n, activation, keep_prob, training=True):
     """tensorflow dropout layer"""
     drop_out = tf.keras.layers.Dropout(1 - keep_prob)
-    result = tf.keras.layers.Dense(n, activation=activation, kernel_initializer=tf.keras.initializers.VarianceScaling(mode='fan_avg'), kernel_regularizer=drop_out)(prev)
+    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    result = tf.keras.layers.Dense(n, activation=activation,
+                                   kernel_initializer=init,
+                                   kernel_regularizer=drop_out)(prev)
     return result
