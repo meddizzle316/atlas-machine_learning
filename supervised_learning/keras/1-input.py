47f494daf62ee6b57136c223a27431a1120a1493
meddizzle316 <meddizzle316@gmail.com>
2024-06-21 12:20:15 -0500

    removed print statement

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -10,14 +10,15 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     l2 = K.regularizers.l2(l2=lambtha)
     input = K.layers.Input(shape=(nx,))
     x = K.layers.Dense(layers[0], activation=activations[0], kernel_regularizer=l2)(input)
+    if len(layers) == 1:
+        return K.models.Model(input, x)
     x = K.layers.Dropout(1 - keep_prob)(x)
     for i in range(1, len(layers)):
         x = K.layers.Dense(layers[i], activation=activations[i], kernel_regularizer=l2)(x)
         if i != len(layers) - 1:
             x = K.layers.Dropout(1 - keep_prob)(x)
-    print(f"this is i {input}")
     # I think because I was 'redefining' i (changed to inputs)
     # in line 12 to an integer in line 14, every time I referenced
     # i after refered to the integer created by the for loop
-    # instead of the inputs
+    # instead of the inputs, didn't rea
     return K.models.Model(input, x)
