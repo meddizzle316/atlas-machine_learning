a285e02a78514090d951868ea481061ddf0f147b
meddizzle316 <meddizzle316@gmail.com>
2024-06-24 11:21:43 -0500

    trying it out

--- supervised_learning/keras/10-weights.py
+++ supervised_learning/keras/10-weights.py
@@ -7,8 +7,6 @@ def save_weights(network, filename, save_format='keras'):
     """saves weights"""
     if filename.split('.')[1] == 'keras':
         network.save_weights(filename)
-    else:
-        network.save_weights(filename + '.' + save_format)
 
 def load_weights(network, filename):
     """loads weights"""
