293cbf3b0b98a18514878c84d084cfd45f7f0ff4
meddizzle316 <meddizzle316@gmail.com>
2024-06-24 11:26:06 -0500

    trying again

--- supervised_learning/keras/10-weights.py
+++ supervised_learning/keras/10-weights.py
@@ -5,8 +5,7 @@ import tensorflow.keras as K
 
 def save_weights(network, filename, save_format='keras'):
     """saves weights"""
-    if filename.split('.')[1] == 'keras':
-        network.save_weights(filename)
+    network.save_weights(filename, save_format=save_format)
 
 def load_weights(network, filename):
     """loads weights"""
