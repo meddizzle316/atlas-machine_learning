060a2af887efa863ad2a481a284527b72790a48e
meddizzle316 <meddizzle316@gmail.com>
2024-06-25 13:22:20 -0500

    trying 2, seems to pass locally

---
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""convolve gray scale"""
+import numpy as np
+
+
+def convolve_grayscale_padding(images, kernel, padding):
+    """convolution with numpy"""
+    m = images.shape[0]
+    # cross correlation
+    # kernel = np.flipud(np.fliplr(kernel))
+    h = images.shape[1]
+    w = images.shape[2]
+
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+
+    out_height = int((images.shape[1] + (2 * (padding[0]))- kh) + 1)
+    out_width = int((images.shape[2] + (2 * (padding[1]))- kw) + 1)
+
+    # print("this is out height", out_height)
+    # print("this is out width", out_width)
+    output = np.zeros((m, out_height, out_width))
+    padded_input = np.pad(images, ((0,0), (padding[0], padding[0]),
+                                           (padding[1], padding[1])))
+    for i in range(out_height):
+        for j in range(out_width):
+            output[:, i, j] += np.sum(padded_input[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+            # calculates the sum over the 2nd and third dimensions, ignoring the first
+
+    return np.array(output)
--- math/convolutions_and_pooling/2-main.py
+++ math/convolutions_and_pooling/2-main.py
@@ -2,7 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-convolve_grayscale_same = __import__('1-convolve_grayscale_same').convolve_grayscale_same
+convolve_grayscale_padding = __import__('2-convolve_grayscale_padding').convolve_grayscale_padding
 
 
 if __name__ == '__main__':
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     dataset = np.load('MNIST.npz')
     images = dataset['X_train']
     print(images.shape)
-    kernel = np.array([[1 ,0, -1, 0], [1, 0, -1, 0], [1, 0, -1, 0]])
-    images_conv = convolve_grayscale_same(images, kernel)
+    kernel = np.array([[1 ,0, -1], [1, 0, -1], [1, 0, -1]])
+    images_conv = convolve_grayscale_padding(images, kernel, (2, 4))
     print(images_conv.shape)
 
     plt.imshow(images[0], cmap='gray')
