0cf0ebc08a966ae03d9c5f96ac4deaac67f8960a
meddizzle316 <meddizzle316@gmail.com>
2024-06-25 15:10:11 -0500

    trying 3 again

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -13,19 +13,22 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
 
     kh = kernel.shape[0]
     kw = kernel.shape[1]
+    # print("this is kh", kh)
+    # print("this is kw", kw)
 
     # out_height = int(((images.shape[1] + (2 * (padding[0])) - kh) / stride[0]) + 1)
     # out_width = int(((images.shape[2] + (2 * (padding[1])) - kw) / stride[1]) + 1
     
-    
+    # print("this is the stride", stride)
     if padding == 'same':
         pad_h = kernel.shape[0] // 2
         pad_w = kernel.shape[1] // 2
-
-        out_height = int((images.shape[1] + (2 * (pad_h))- kh) + 1)
-        out_width = int((images.shape[2] + (2 * (pad_w))- kw) + 1)
+        # print(pad_h)
+        # print(pad_w)
+        out_height = int((images.shape[1] + (2 * (pad_h))- kh)/stride[0]) + 1
+        out_width = int((images.shape[2] + (2 * (pad_w))- kw)/stride[1]) + 1
         padded_input = np.pad(images, ((0,0), (pad_h, pad_h),
-                                           pad_w, pad_w))
+                                           (pad_w, pad_w)))
     elif padding == 'valid':
         out_height = int((images.shape[1] - kh)/ stride[0]) + 1
         out_width = int((images.shape[2] - kw)/ stride[1]) + 1
@@ -35,15 +38,23 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
         pad_h = padding[0]
         pad_w = padding[1]
         
-        out_height = int((images.shape[1] + (2 * (pad_h))- kh) + 1)
-        out_width = int((images.shape[2] + (2 * (pad_w))- kw) + 1)
+        out_height = int((images.shape[1] + (2 * (pad_h))- kh)/stride[0]) + 1
+        out_width = int((images.shape[2] + (2 * (pad_w))- kw)/stride[1]) + 1
         padded_input = np.pad(images, ((0,0), (pad_h, pad_h),
                                            pad_w, pad_w))
 
     output = np.zeros((m, out_height, out_width))
+    # print("this is out height", out_height)
+    # print("this is out width", out_width)
     for i in range(0, out_height):
         for j in range(0, out_width):
-            output[:, i, j] += np.sum(padded_input[:, i * stride[0]:i*stride[0]+kh, j * stride[1]:j *stride[1]+kw] * kernel, axis=(1, 2))
+            # if (i * stride[0] + kh) - (i * stride[0]) != kernel.shape[0]:
+            #     pass
+            # if (j * stride[1] + kw) - (j * stride[1]) != kernel.shape[1]:
+            #     pass
+            # else:
+            output[:, i, j] += np.sum(padded_input[:, i * stride[0]:(i*stride[0])+kh, j * stride[1]:(j * stride[1]) + kw] * kernel, axis=(1, 2))
             # calculates the sum over the 2nd and third dimensions, ignoring the first
+
 
     return np.array(output)
