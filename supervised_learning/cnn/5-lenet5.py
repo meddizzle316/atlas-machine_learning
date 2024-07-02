1cd7ff7f1471bf1683d248f36e4d81e7d2973bbd
meddizzle316 <meddizzle316@gmail.com>
2024-07-02 08:22:30 -0500

    pyfix

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -11,15 +11,21 @@ def lenet5(input):
     m, h, w, c = input.shape
 
     he_normal = K.initializers.VarianceScaling(scale=2.0, seed=0)
-    x = K.layers.Conv2D(6, (5, 5), activation='relu', input_shape=(m, h, w, c), padding='same', kernel_initializer=he_normal)(input)
+    x = K.layers.Conv2D(6, (5, 5),
+                        activation='relu',
+                        input_shape=(m, h, w, c),
+                        padding='same', kernel_initializer=he_normal)(input)
     x = K.layers.MaxPooling2D(pool_size=2, strides=2)(x)
-    x = K.layers.Conv2D(16, (5, 5), padding='valid', kernel_initializer=he_normal, activation='relu')(x)
+    x = K.layers.Conv2D(16, (5, 5),
+                        padding='valid',
+                        kernel_initializer=he_normal,
+                        activation='relu')(x)
     x = K.layers.MaxPooling2D(pool_size=2, strides=2)(x)
     x = K.layers.Flatten()(x)
     x = K.layers.Dense(120, activation='relu', kernel_initializer=he_normal)(x)
     x = K.layers.Dense(84, activation='relu', kernel_initializer=he_normal)(x)
-    x = K.layers.Dense(10, activation='softmax', kernel_initializer=he_normal)(x)
-
+    x = K.layers.Dense(10, activation='softmax',
+                       kernel_initializer=he_normal)(x)
 
     model = K.models.Model(input, x)
 
