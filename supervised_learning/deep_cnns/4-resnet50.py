7866a77e643a24157ad598c9d511058ef281d170
meddizzle316 <meddizzle316@gmail.com>
2024-07-08 15:06:31 -0500

    once more

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """resnet50 in keras"""
 from tensorflow import keras as K
-
+identity_block = __import__('2-identity_block').identity_block
+projection_block = __import__('3-projection_block').projection_block
 
 def resnet50():
     """builds resnet50 in keras"""
