4e9fc0bb60cbcc133abe798733f5d78940351348
meddizzle316 <meddizzle316@gmail.com>
2024-07-08 10:12:18 -0500

    fixed the order I think?

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -7,6 +7,8 @@ def inception_block(A_prev, filters):
     """inception block with keras"""
     F1, F3R, F3, F5R, F5, FPP = filters
 
+    layer_4 = K.layers.Conv2D(F1, (1, 1), padding='same', activation='relu')(A_prev)
+
     layer_1 = K.layers.Conv2D(F3R, (1, 1), padding='same', activation='relu')(A_prev)
     layer_1 = K.layers.Conv2D(F3, (3, 3), padding='same', activation='relu')(layer_1)
 
@@ -16,8 +18,8 @@ def inception_block(A_prev, filters):
     layer_3 = K.layers.MaxPooling2D((3, 3), strides=(1, 1), padding='same')(A_prev)
     layer_3 = K.layers.Conv2D(FPP, (1, 1), padding='same', activation='relu')(layer_3)
 
-    layer_4 = K.layers.Conv2D(F1, (1, 1), padding='same', activation='relu')(A_prev)
 
-    mid_1 = K.layers.concatenate([layer_1, layer_2, layer_3, layer_4], axis=3)
+
+    mid_1 = K.layers.concatenate([layer_4, layer_1, layer_2, layer_3], axis=3)
 
     return mid_1
