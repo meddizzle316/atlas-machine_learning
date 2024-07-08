d6e73e6c3a058b8f4eea31c64c819296ca9c6057
meddizzle316 <meddizzle316@gmail.com>
2024-07-08 10:15:04 -0500

    pyfix

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -3,23 +3,21 @@
 from tensorflow import keras as K
 
 
-def inception_block(A_prev, filters):
+def inception_block(X, filters):
     """inception block with keras"""
     F1, F3R, F3, F5R, F5, FPP = filters
 
-    layer_4 = K.layers.Conv2D(F1, (1, 1), padding='same', activation='relu')(A_prev)
+    l4 = K.layers.Conv2D(F1, (1, 1), padding='same', activation='relu')(X)
 
-    layer_1 = K.layers.Conv2D(F3R, (1, 1), padding='same', activation='relu')(A_prev)
-    layer_1 = K.layers.Conv2D(F3, (3, 3), padding='same', activation='relu')(layer_1)
+    l1 = K.layers.Conv2D(F3R, (1, 1), padding='same', activation='relu')(X)
+    l1 = K.layers.Conv2D(F3, (3, 3), padding='same', activation='relu')(l1)
 
-    layer_2 = K.layers.Conv2D(F5R, (1, 1), padding='same', activation='relu')(A_prev)
-    layer_2 = K.layers.Conv2D(F5, (5, 5), padding='same', activation='relu')(layer_2)
+    l2 = K.layers.Conv2D(F5R, (1, 1), padding='same', activation='relu')(X)
+    l2 = K.layers.Conv2D(F5, (5, 5), padding='same', activation='relu')(l2)
 
-    layer_3 = K.layers.MaxPooling2D((3, 3), strides=(1, 1), padding='same')(A_prev)
-    layer_3 = K.layers.Conv2D(FPP, (1, 1), padding='same', activation='relu')(layer_3)
+    l3 = K.layers.MaxPooling2D((3, 3), strides=(1, 1), padding='same')(X)
+    l3 = K.layers.Conv2D(FPP, (1, 1), padding='same', activation='relu')(l3)
 
-
-
-    mid_1 = K.layers.concatenate([layer_4, layer_1, layer_2, layer_3], axis=3)
+    mid_1 = K.layers.concatenate([l4, l1, l2, l3], axis=3)
 
     return mid_1
