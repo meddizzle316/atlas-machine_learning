7fccc3378c7da484622a9129f5992980a06736f4
meddizzle316 <meddizzle316@gmail.com>
2024-07-08 15:08:36 -0500

    actually kind of works?

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -4,6 +4,8 @@ from tensorflow import keras as K
 identity_block = __import__('2-identity_block').identity_block
 projection_block = __import__('3-projection_block').projection_block
 
+
 def resnet50():
     """builds resnet50 in keras"""
-    return K.applications.resnet50.ResNet50()
+    return K.applications.resnet50.ResNet50(
+        weights=K.initializers.HeNormal(seed=0))
