738a852aea8c8fe09dd53c06a4607d3b3163c414
meddizzle316 <meddizzle316@gmail.com>
2024-07-09 11:35:24 -0500

    7 is passing locally

---
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""densenet121 in keras"""
+from tensorflow import keras as K
+dense_block = __import__('5-dense_block').dense_block
+transition_layer = __import__('6-transition_layer').transition_layer
+
+
+def densenet121(growth_rate=32, compression=1.0):
+    """densenet121 in keras"""
+    h_n = K.initializers.HeNormal(seed=0)
+    i = K.Input(shape=(224, 224, 3))
+    x = K.layers.BatchNormalization()(i)
+    x = K.layers.Activation('relu')(x)
+    x = K.layers.Conv2D(64, (7, 7), strides=(2, 2),
+                        padding='same',
+                        kernel_initializer=h_n)(x)
+    x = K.layers.MaxPooling2D(pool_size=(3, 3),
+                              strides=(2, 2),
+                              padding='same')(x)
+    x, _ = dense_block(x, 56, growth_rate, 6)
+    x, _ = transition_layer(x, 56, compression)
+    x, _ = dense_block(x, 28, growth_rate, 12)
+    x, _ = transition_layer(x, 28, compression)
+    x, _ = dense_block(x, 14, growth_rate, 24)
+    x, _ = transition_layer(x, 14, compression)
+    x, _ = dense_block(x, 7, growth_rate, 16)
+    x = K.layers.AveragePooling2D(pool_size=(7, 7),
+                                        padding='same')(x)
+    x = K.layers.Dense(1000, activation='softmax',
+                       kernel_initializer=h_n)(x)
+    return K.models.Model(i, x)
