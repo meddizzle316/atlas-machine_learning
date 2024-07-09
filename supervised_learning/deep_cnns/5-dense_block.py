9d6ccad6c60c97c66c97e8d689592f68717ac702
meddizzle316 <meddizzle316@gmail.com>
2024-07-09 09:27:21 -0500

    trying 5

---
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""dense block in keras"""
+from tensorflow import keras as K
+
+
+def dense_block(X, nb_filters, growth_rate, layers):
+    """dense block in keras"""
+
+    h_n = K.initializers.HeNormal(seed=0)
+    x = X
+    x_prev = X
+    for i in range(layers):
+        x = K.layers.BatchNormalization()(x)
+        x = K.layers.Activation('relu')(x)
+        x = K.layers.Conv2D(growth_rate * 4, (1, 1), padding='same')(x)
+        x = K.layers.BatchNormalization()(x)
+        x = K.layers.Activation("relu")(x)
+        x = K.layers.Conv2D(32, (3, 3), padding='same')(x)
+        x = K.layers.Concatenate()([x_prev, x])
+        x_prev = x
+    return x, x.shape[3]
