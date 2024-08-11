3c1db7917d66365fe837fa3c084b19ae8cd93fbd
meddizzle316 <meddizzle316@gmail.com>
2024-08-11 14:35:45 -0500

    finally got it

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -37,6 +37,8 @@ class Yolo():
         class_probs = []
         image_h = image_size[0]
         image_w = image_size[1]
+        input_layer = self.model.input
+        input_w, input_h = input_layer.shape[1:3]
 
         i = 0
         for output in outputs:
@@ -51,14 +53,11 @@ class Yolo():
             sig_conf = self.sigmoid(confidence)
             # print("this is sig confidence", sig_conf)
             confidence_list.append(sig_conf)
-
             class_probs.append(self.sigmoid(box[:, :, :, 5:]))
 
             gh = box.shape[0]
             gw = box.shape[1]
             num_anchors = output.shape[2]
-            # print("this is shape", gridH, " of i", i)
-            # print("this is the number of anchors", num_anchors)
 
             cx = np.arange(gw).reshape(1, gw)
             cx = np.repeat(cx, gh, axis=0)
@@ -67,42 +66,23 @@ class Yolo():
 
             cy = np.repeat(cy[..., np.newaxis], num_anchors, axis=2)
             cx = np.repeat(cx[..., np.newaxis], num_anchors, axis=2)
-            # cy = np.expand_dims(cy, -1)
-            # cx = np.expand_dims(cx, -1)
-
-            # pred_x = (self.sigmoid(x) + cx) / gridW
-            # pred_y = (self.sigmoid(y) + cy) / gridH
-            # pred_w = (np.exp(w) * self.anchors[i, :, 0]) / image_width
-            # pred_h = (np.exp(h) * self.anchors[i, :, 1]) / image_height
 
             pred_x = (self.sigmoid(box[..., 0]) + cx) / gw
             pred_y = (self.sigmoid(box[..., 1]) + cy) / gh
             # print("this is the anchor", self.anchors[i, :, 0], "at ", i)
-            pred_w = (np.exp(box[..., 2]) * self.anchors[i, :, 0]) / image_w
-            pred_h = (np.exp(box[..., 3]) * self.anchors[i, :, 1]) / image_h
+            pred_w = (np.exp(box[..., 2]) * self.anchors[i, :, 0]) / input_w
+            pred_h = (np.exp(box[..., 3]) * self.anchors[i, :, 1]) / input_h
+            # input w and input h should be 416
+            # they are the 'inputs' to the Yolo model itself
+            # the input layer of the yolov3 model
+            # is 416, 416
+            # this is not the image_size
 
             boxes[i][..., 0] = (pred_x - (pred_w * 0.5)) * image_w
             boxes[i][..., 1] = (pred_y - (pred_h * 0.5)) * image_h
             boxes[i][..., 2] = (pred_x + (pred_w * 0.5)) * image_w
             boxes[i][..., 3] = (pred_y + (pred_h * 0.5)) * image_h
-            # boxes[i] = boxes[i].astype(np.int32)
-            # x1 = (pred_x - (pred_w * 0.5)) * image_width
-            # y1 = (pred_y - (pred_h * 0.5)) * image_height
-            # x0 = (pred_x + (pred_w * 0.5)) * image_width
-            # y0 = (pred_y + (pred_h * 0.5)) * image_height
 
-            # print("x1 shape", x1.shape)
-            # print("y1 shape", y1.shape)
-            # print("x0 shape", x0.shape)
-            # print("y0 shape", y0.shape)
-
-            # print("x1 shape", x1.shape)
-            # print("y1 shape", y1.shape)
-
-            # box_cord = np.concatenate([x1, y1, x0, y0], axis=-1)
-            # box_cord = np.reshape(box_cord, (gridH, gridW, num_anchors, 4))
-            # print("shape of box cord", box_cord.shape)
-            # boxes.append(box_cord)
             i += 1
 
         return [boxes, confidence_list, class_probs]
