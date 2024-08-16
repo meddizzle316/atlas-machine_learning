c08c9db19aeff863c6a7957de329e47b027e3d45
meddizzle316 <meddizzle316@gmail.com>
2024-08-16 12:21:16 -0500

    normalizing

--- supervised_learning/object_detection/5-yolo.py
+++ supervised_learning/object_detection/5-yolo.py
@@ -178,10 +178,11 @@ class Yolo():
         img_list = []
         img_org_size = []
         for image in images:
-            img_list.append(cv2.resize(image, (input_w, input_h), interpolation=cv2.INTER_CUBIC))
+            resize_image = cv2.resize(image, (input_w, input_h), interpolation=cv2.INTER_CUBIC)
+            resize_image /= 255.0
+            img_list.append(resize_image)
             img_org_size.append(image.shape[:2])
 
-        img_list /= 255.0
         image_arr = np.array(img_list)
         org_size_arr = np.array(img_org_size)
         return image_arr, org_size_arr
