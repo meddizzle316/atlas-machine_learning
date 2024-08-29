8993c431496521620f23f21a955cef3705a4e245
meddizzle316 <meddizzle316@gmail.com>
2024-08-29 10:17:09 -0500

    once more into the breach

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""doing determinants of matrices"""
+
+
+def getMatrixMinor(m, i, j):
+    """gets minor of given element of matrix"""
+    return [row[:j] + row[j + 1:] for row in (m[:i] + m[i + 1:])]
+
+
+def determinant(matrix):
+    """gets determinant of given matrix"""
+
+    if len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    elif len(matrix[0]) == 0:
+        return 1
+    elif len(matrix[0]) == 1:
+        return matrix[0][0]
+    elif len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    deter = 0
+    for c in range(len(matrix)):
+        deter += ((-1)**c) * matrix[0][c] * \
+            determinant(getMatrixMinor(matrix, 0, c))
+    return deter
