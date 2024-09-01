982b13af3c0bac7fb101962b7a73041c94d47f13
meddizzle316 <meddizzle316@gmail.com>
2024-09-01 13:04:21 -0500

    new error handling

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -20,6 +20,8 @@ def determinant(matrix):
         for row in matrix:
             if len(matrix) != len(row):
                 raise ValueError("matrix must be a square matrix")
+    else:
+        raise TypeError("matrix must be a list of lists")
 
     if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
