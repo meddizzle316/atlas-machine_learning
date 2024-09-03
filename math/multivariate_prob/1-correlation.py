31a52cff0ddc30adc17c25abf51bebfc74f6de3d
meddizzle316 <meddizzle316@gmail.com>
2024-09-02 21:56:52 -0500

    going for last 2d check

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -7,8 +7,11 @@ def correlation(C):
     """gets correclation matrix"""
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    d1, d2 = C.shape
-    if not d1 == d2:
+    try:
+        d1, d2 = C.shape
+        if not d1 == d2:
+            raise TypeError("C must be a 2D numpy.ndarray")
+    except ValueError:
         raise TypeError("C must be a 2D numpy.ndarray")
     v = np.sqrt(np.diag(C))
     outer_v = np.outer(v, v)
