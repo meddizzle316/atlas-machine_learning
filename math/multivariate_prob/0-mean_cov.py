b24527e74d398513ad746c5b93dfe4d4ade1bb22
meddizzle316 <meddizzle316@gmail.com>
2024-09-02 21:39:20 -0500

    attempting mean check

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -5,7 +5,11 @@ import numpy as np
 
 def mean_cov(X):
     """calculates mean and covariance of given dataset"""
+    if not isinstance(X, np.ndarray):
+        raise TypeError("X must be a 2D numpy.ndarray")
     n, d = X.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
 
     cov = np.zeros((d, d))
     for i in range(d):
@@ -14,7 +18,8 @@ def mean_cov(X):
         for j in range(d):
             mean_j = np.sum(X[:, j]) / n
 
-            cov[i, j] = np.sum((X[:, i] - mean_i) * (X[:, j] - mean_j)) / (n - 1)
+            cov[i, j] = np.sum((X[:, i] - mean_i) *
+                               (X[:, j] - mean_j)) / (n - 1)
 
     mean = np.mean(X, axis=0)
     resh_mean = np.reshape(mean, (1, -1))
