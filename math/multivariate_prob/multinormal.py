872373b35133c8d40e9cda76bb56cfa19ca43bcc
meddizzle316 <meddizzle316@gmail.com>
2024-09-03 09:09:17 -0500

    attempting task 2

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -4,17 +4,20 @@ import numpy as np
 
 
 def mean_cov(X):
-    """calculates mean and covariance of given dataset"""
+    """calculates mean and covariance of given dataset
+        Arg: X -- is numpy arry of d, n
+    """
     if not isinstance(X, np.ndarray):
         raise TypeError("data must be a 2D numpy.ndarray")
     if not X.ndim >= 2:
         raise TypeError("data must be a 2D numpy.ndarray")
-    n, d = X.shape
-    if d < 2:
+    d, n = X.shape
+    if n < 2:
         raise ValueError("data must contain multiple data points")
     mean = np.mean(X, axis=-1)
     resh_mean = np.reshape(mean, (-1, 1))
     cov = np.zeros((d, d))
+    X = X.T
     for i in range(d):
 
         mean_i = np.sum(X[:, i]) / n
@@ -29,7 +32,8 @@ def mean_cov(X):
 
 class MultiNormal():
     def __init__(self, data):
-        """init function"""
+        """init function for MultNormal class
+        sets mean and covariance of given dataset"""
         mean, cov = mean_cov(data)
         self.mean = mean
         self.cov = cov
