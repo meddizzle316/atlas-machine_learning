24b789b3ac00c44289e8714f17d3e3ee8e4992f5
meddizzle316 <meddizzle316@gmail.com>
2024-09-03 08:46:10 -0500

    attempting task 2

---
+++ math/multivariate_prob/multinormal.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""creating multinormal class"""
+import numpy as np
+
+
+def mean_cov(X):
+    """calculates mean and covariance of given dataset"""
+    if not isinstance(X, np.ndarray):
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if not X.ndim >= 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    n, d = X.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+
+    cov = np.zeros((d, d))
+    for i in range(d):
+
+        mean_i = np.sum(X[:, i]) / n
+        for j in range(d):
+            mean_j = np.sum(X[:, j]) / n
+
+            cov[i, j] = np.sum((X[:, i] - mean_i) *
+                               (X[:, j] - mean_j)) / (n - 1)
+
+    mean = np.mean(X, axis=0)
+    resh_mean = np.reshape(mean, (1, -1))
+    return resh_mean, cov
+class MultiNormal():
+    def __init__(self, data):
+        """init function"""
+        mean, cov = mean_cov(data)
+        self.mean = mean
+        self.cov = cov
+
