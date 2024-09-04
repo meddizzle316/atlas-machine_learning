f45fd37c5819f31cb7aaaffc0b2893bfb0a3f345
meddizzle316 <meddizzle316@gmail.com>
2024-09-04 08:52:48 -0500

    trying pyfix and doc

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -85,13 +85,13 @@ def intersection(x, n, P, Pr):
     if not P.ndim == 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if not np.all((0 <= P) & (P <= 1)):
-        raise ValueError("All values in {P} must be in the range [0, 1]")
+        raise ValueError("All values in P must be in the range [0, 1]")
     if not isinstance(Pr, np.ndarray):
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if not Pr.shape == P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if not np.all((0 <= Pr) & (Pr <= 1)):
-        raise ValueError("All values in {Pr} must be in the range [0, 1]")
+        raise ValueError("All values in Pr must be in the range [0, 1]")
     if not np.sum(Pr) == 1:
         raise ValueError("Pr must sum to 1")
 
