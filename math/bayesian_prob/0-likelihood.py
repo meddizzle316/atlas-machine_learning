4c0e5787781213c0bf2776edb49a4dac26c134f9
meddizzle316 <meddizzle316@gmail.com>
2024-09-04 04:50:42 -0500

    trying pyfix and doc

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -4,25 +4,37 @@ import numpy as np
 
 
 def pmf(k, n, p):
-    """probability mass function of binomial"""
+    """probability mass function of binomial
+    K: corresponds to x in likelihood function
+    n: corresponds to n
+    P: corresponds to P
+    """
     if k < 0:
         return 0
     k = int(k)
-    first_factor = np.math.factorial(n) / (np.math.factorial(n - k) * (np.math.factorial(k)))
+    first_factor = np.math.factorial(
+        n) / (np.math.factorial(n - k) * (np.math.factorial(k)))
     second_factor = (p ** k) * ((1 - p) ** (n - k))
     return first_factor * second_factor
 
 
 def likelihood(x, n, P):
-    """gets likelihood of x for n trials with P probability (as threshold)"""
+    """gets likelihood of x for n trials with P probability (as threshold)
+    ARgs
+    X: the given number
+    n: number of trials
+    P: the given probability in numpy format
+    """
     if not isinstance(n, int):
         raise ValueError("n must be a positive integer")
     if n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int):
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray):
@@ -37,6 +49,4 @@ def likelihood(x, n, P):
         prob = pmf(x, n, p)
         scratch_likeli.append(prob)
     np_array = np.array(scratch_likeli)
-    # np_likeli = np.array_str(np_array, precision=8)
     return np_array
-
