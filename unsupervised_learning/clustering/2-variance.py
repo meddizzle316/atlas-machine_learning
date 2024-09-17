162c00339d992ba196eb86d49064ecb8d7d0de4c
meddizzle316 <meddizzle316@gmail.com>
2024-09-17 14:18:20 -0500

    trying 2

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -9,9 +9,14 @@ def variance(X, C):
          You are not allowed to use any loops
         Returns: var, or None on failure
         var is the total variance"""
+    try:
+        n, d = X.shape
+        k, d1 = C.shape
+    except Exception:
+        return None
+
 
     # Calculate squared distances
-    # Assign each data point to its nearest centroid
     distances = np.min(np.linalg.norm(X[:, None] - C, axis=2), axis=1)
 
     # Calculate the squared distances
