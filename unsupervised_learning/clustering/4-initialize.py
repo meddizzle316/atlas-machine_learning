bd2849a47f9a5ca2a964307e431f9fd88a132785
meddizzle316 <meddizzle316@gmail.com>
2024-09-18 10:04:37 -0500

    point scraping

---
+++ unsupervised_learning/clustering/4-initialize.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""initializes variables for Gassian Mixture model"""
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """X is a numpy.ndarray of shape (n, d)
+    containing the data set
+    k is a positive integer containing the number of clusters
+    You are not allowed to use any loops
+
+    Returns: pi, m, S, or None, None, None on failure
+        pi is a numpy.ndarray of shape (k,) containing
+        the priors for each cluster, initialized evenly
+        m is a numpy.ndarray of shape (k, d) containing
+        the centroid means for each cluster, initialized
+        with K-means
+        S is a numpy.ndarray of shape (k, d, d) containing
+        the covariance matrices for each cluster, initialized
+        as identity matrices
+        You should use kmeans = __import__('1-kmeans').kmeans"""
+
+    if not isinstance(X, np.ndarray) or not isinstance(k, int):
+        return None, None, None
+
+    if k <= 0 or X.ndim != 2:
+        return None, None, None
+
+    return None, None, None
