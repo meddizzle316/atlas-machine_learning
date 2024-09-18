6c7bc69165adfa16baf02cfa680bf64b9e83971a
meddizzle316 <meddizzle316@gmail.com>
2024-09-18 10:29:17 -0500

    point scraping

---
+++ unsupervised_learning/clustering/5-pdf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""pdf of gassian distribution"""
+import numpy as np
+
+
+def pdf(x, m, s):
+    """pdf of gassian distribution"""
+
+    if not isinstance(
+        x, np.ndarray) or not isinstance(
+        m, np.ndarray) or not isinstance(
+            s, np.ndarray):
+        return None
+    if not x.ndim != 2 or not m.ndim != 1 or not s.ndim != 2:
+        return None
+    e = 2.7182818285
+    pi = 3.1415926536
+
+    # mean = np.mean(x)
+    mean = m
+    stddev = np.std(x)
+    print('this is the std', stddev)
+
+    """gets pdf for given x"""
+    first_factor = 1 / (((stddev ** 2) * (2 * pi)) ** 0.5)
+    second_factor = e ** ((-(x - mean) ** 2) / ((2 * (stddev ** 2))))
+    return (first_factor * second_factor)
