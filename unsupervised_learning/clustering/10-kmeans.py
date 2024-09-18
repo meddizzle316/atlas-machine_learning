7423fd3008ea15957ef273c0a643f597af8364c6
meddizzle316 <meddizzle316@gmail.com>
2024-09-18 13:23:30 -0500

    actually trying for this one

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""uses scipy to run kmeans"""
+import numpy as np
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """performs k-means on dataset"""
+    if not isinstance(X, np.ndarray) or not isinstance(k, int):
+        return None, None
+    kmeans = sklearn.cluster.KMeans(n_clusters=k).fit(X)
+    return kmeans.cluster_centers_, kmeans.labels_
