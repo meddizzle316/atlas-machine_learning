80500b8d82a345bc42ed8c591701106b887a2ce3
meddizzle316 <meddizzle316@gmail.com>
2024-09-23 17:13:31 -0500

    2 is passing locally, mostly

---
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""checks for absorbing states"""
+import numpy as np
+
+
+def absorbing(p):
+    """checks for absorbing states"""
+
+
+    # if not isinstance(p, np.ndarray):
+    #     return None
+    n, m = p.shape
+    # if not p.shape == (n, n):
+    #     return None
+    if np.any(p == 1):
+        return True
+    elif np.linalg.det(p) == 0:
+        return False
+    else:
+        return False
