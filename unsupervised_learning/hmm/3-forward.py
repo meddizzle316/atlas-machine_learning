96ee9fa5a06466a47621389141a55a2ad135a8bf
meddizzle316 <meddizzle316@gmail.com>
2024-09-25 12:29:00 -0500

    trying out 3

---
+++ unsupervised_learning/hmm/3-forward.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""forward alg for hidden markov model"""
+from asyncio.trsock import TransportSocket
+
+import numpy as np
+
+def forward(Observation, Emission, Transition, Initial):
+    """forward alg for hidden markov model"""
+    T = len(Observation)
+    N = Emission.shape[0]
+    M = Emission.shape[0]
+
+    F = np.zeros((N, T))
+    F[:, 0] = Initial[:, 0] * Emission[:, Observation[0]]
+
+    for t in range(1, T):
+        for i in range(N):
+            F[i, t] = np.sum(F[:, t-1] * Transition[:, i] * Emission[i, Observation[t]])
+
+    P = np.sum(F[:, T-1])
+    return P, F
