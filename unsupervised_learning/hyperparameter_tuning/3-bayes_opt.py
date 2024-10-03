56c0f8e12806ea56e482c8fa7f6d608122d8a6f6
meddizzle316 <meddizzle316@gmail.com>
2024-10-03 10:57:57 -0500

    trying 3, not exactly but close, might need to change method of generating acquisition samples

---
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+performs bayesian optimization on a noiseless
+1d gaussian process
+"""
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+
+class BayesianOptimization:
+    """class for bayesian opt operation"""
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """init operation"""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
+        self.xsi = xsi
+        self.minimize= minimize
+        min, max = bounds
+        self.X_s = np.random.uniform(min, max, (ac_samples, 1))
+
+
