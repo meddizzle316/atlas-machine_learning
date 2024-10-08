4b766250beda657b28a1b31673c5e339260cdf01
meddizzle316 <meddizzle316@gmail.com>
2024-10-08 10:40:22 -0500

    pyfix

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -6,16 +6,25 @@ performs bayesian optimization on a noiseless
 import numpy as np
 GP = __import__('2-gp').GaussianProcess
 
+
 class BayesianOptimization:
     """class for bayesian opt operation"""
 
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+    def __init__(
+            self,
+            f,
+            X_init,
+            Y_init,
+            bounds,
+            ac_samples,
+            l=1,
+            sigma_f=1,
+            xsi=0.01,
+            minimize=True):
         """init operation"""
         self.f = f
         self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
         self.xsi = xsi
-        self.minimize= minimize
+        self.minimize = minimize
         min, max = bounds
         self.X_s = np.linspace(min, max, ac_samples).reshape(-1, 1)
-
-
