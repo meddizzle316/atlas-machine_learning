8b305244bd69b25f296f6f70ec7eb7f965e154ee
meddizzle316 <meddizzle316@gmail.com>
2024-10-12 14:29:04 -0500

    I think I have 0

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""creates an autoencoder model"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """creates an autoencoder model
+    returns
+    encoder
+    decoder
+    auto -- the full model"""
+
+    encoder_input = keras.layers.Input(shape=(input_dims,))
+    x = keras.layers.Dense(hidden_layers[0], activation='relu')(encoder_input)
+
+    for num_nodes in range(1, len(hidden_layers)):
+        x = keras.layers.Dense(hidden_layers[num_nodes], activation='relu')(x)
+    x = keras.layers.Dense(latent_dims, activation='relu')(x)
+
+    encoder = keras.models.Model(inputs=encoder_input, outputs=x)
+    encoder.summary()
+
+    # decoder_input = keras.layers.Input(shape=(latent_dims,))
+    encoder_output = encoder.output
+    y = keras.layers.Dense(hidden_layers[-1], activation='relu')(encoder_output)
+    for index in range(len(hidden_layers)-2, -2, -1):
+        if index == -1:
+          y = keras.layers.Dense(input_dims, activation='sigmoid')(y)
+        # elif index == 0:
+        #    y = keras.layers.Dense(hidden_layers[index], activation='sigmoid')(y)
+        else:
+           y = keras.layers.Dense(hidden_layers[index], activation='relu')(y)
+
+    decoder = keras.models.Model(inputs=encoder_output, outputs=y)
+    decoder.summary()
+    autoencoder = keras.models.Model(inputs=encoder.input, outputs=y)
+    print(autoencoder.summary())
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, autoencoder
