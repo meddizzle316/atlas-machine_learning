3d4a81d48ca047124dd92b5eb5c893bab52e1f8d
meddizzle316 <meddizzle316@gmail.com>
2024-11-18 11:28:47 -0600

    seeing if imports work

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""performs basic bag of words"""
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+
+
+def bag_of_words(sentences, vocab=None):
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    feature_names = vectorizer.get_feature_names_out()
+    X = X.toarray()
+    return X, feature_names
