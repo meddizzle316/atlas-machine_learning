2894d41aa758a5a7b22fb33496b52ca26fc306c7
meddizzle316 <meddizzle316@gmail.com>
2024-11-18 12:00:47 -0600

    first go at 2 with gensin.word2vec model

---
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""performs gensin Word2Vec model"""
+from gensim.models import Word2Vec
+import numpy as np
+
+
+def word2vec_model(sentences, vector_size=100,
+                   min_count=5, window=5, negative=5,
+                   cbow=True, epochs=5, seed=0,
+                   workers=1):
+    """makes a word2vec model"""
+    model = Word2Vec(
+        sentences=sentences,
+        vector_size=vector_size,
+        min_count=min_count,
+        workers=workers,
+        window=window,
+        seed=seed,
+        negative=negative,
+        epochs=epochs,
+        sg=1 if not cbow else 0
+    )
+
+    model.train(sentences, total_examples=1, epochs=model.epochs)
+    return model
