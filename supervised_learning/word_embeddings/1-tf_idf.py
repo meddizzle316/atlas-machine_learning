784f13338b786336ccce10016930fdbc9e985ae2
meddizzle316 <meddizzle316@gmail.com>
2024-11-18 11:41:46 -0600

    first go at 1 with sklearn module

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""performs basic tf_idf"""
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+
+def tf_idf(sentences, vocab=None):
+    """does tf_idf for each sentence"""
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    tfidf_matrix = vectorizer.fit_transform(sentences)
+    feature_names = vectorizer.get_feature_names_out()
+    tfidf_matrix = tfidf_matrix.toarray()
+    return tfidf_matrix, feature_names
