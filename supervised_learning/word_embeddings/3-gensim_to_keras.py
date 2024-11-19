dda92fb706e5e0c7a39fa88d3e181c1d911ef220
meddizzle316 <meddizzle316@gmail.com>
2024-11-19 15:02:57 -0600

    trying out another versino of 3

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -5,14 +5,30 @@ import tensorflow as tf
 
 def gensim_to_keras(model):
     """converts gensim word2vec model to keras model"""
-    embedding_matrix = model.wv.vectors
-    vocab_size, embedding_dim = embedding_matrix.shape
+    # embedding_matrix = model.wv.vectors
+    # vocab_size, embedding_dim = embedding_matrix.shape
+    #
+    # embedding_layer = tf.keras.layers.Embedding(
+    #     input_dim=vocab_size,
+    #     output_dim=embedding_dim,
+    #     weights=[embedding_matrix],
+    #     trainable=True,
+    # )
+    #
+    # return embedding_layer
+
+    vocab_size = len(model.wv)
+    vector_size = model.wv.vector_size
+
+    embedding_matrix = tf.Variable(tf.zeros((vocab_size + 1, vector_size)), trainable=False)
+
+    for i, word in enumerate(model.wv.index_to_key):
+        embedding_matrix = embedding_matrix[i + 1].assign(model.wv[word])
 
     embedding_layer = tf.keras.layers.Embedding(
-        input_dim=vocab_size,
-        output_dim=embedding_dim,
-        weights=[embedding_matrix],
-        trainable=True,
+        input_dim=vocab_size + 1,
+        output_dim=vector_size,
+        embeddings_initializer=tf.keras.initializers.Constant(embedding_matrix.numpy()),
+        trainable=True
     )
-
     return embedding_layer
