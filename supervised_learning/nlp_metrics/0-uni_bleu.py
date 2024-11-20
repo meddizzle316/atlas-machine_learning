d778da2a48e70ac8f6b30ef025d93ba153e243cd
meddizzle316 <meddizzle316@gmail.com>
2024-11-20 09:57:12 -0600

    pyfix

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -16,12 +16,19 @@ def uni_bleu(references, sentence):
         overlap = sum((ref_count & sentence_count).values())
         ref_length = len(reference)
 
-        if abs(ref_length - len(sentence)) < abs(best_reference_length - len(sentence)):
+        if abs(
+                ref_length -
+                len(sentence)) < abs(
+                best_reference_length -
+                len(sentence)):
             best_reference_length = ref_length
             best_overlap = overlap
 
     precision = best_overlap / len(sentence) if len(sentence) > 0 else 0
 
-    brevity_penalty = math.exp(1 - best_reference_length / len(sentence)) if len(sentence) < best_reference_length else 1
+    brevity_penalty = math.exp(
+        1 -
+        best_reference_length /
+        len(sentence)) if len(sentence) < best_reference_length else 1
 
     return (brevity_penalty * precision)
