c7558548aa5f95f11113e711bf30578cc27a905a
meddizzle316 <meddizzle316@gmail.com>
2025-01-07 15:27:20 -0600

    pyfix

---
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""does epsilon greedy operations"""""
+import numpy as np
+
+def epsilon_greedy(Q, state, epsilon):
+    """value function"""
+    p = np.random.uniform()
+    if p < epsilon:
+        action = np.random.randint(4)
+    else:
+        action = np.argmax(Q[state,:]) # following the Q table
+
+    return action
