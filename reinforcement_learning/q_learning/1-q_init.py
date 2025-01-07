f7470d3ea9a631907aea5614a8bc8f1b3234f70b
meddizzle316 <meddizzle316@gmail.com>
2025-01-07 15:14:06 -0600

    pyfix

--- reinforcement_learning/q_learning/1-q_init.py
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -2,9 +2,10 @@
 """initializes q table"""
 import numpy as np
 
+
 def q_init(env):
     """initilizaes q table"""
-    action_space_size =  env.action_space.n
+    action_space_size = env.action_space.n
     state_space_size = env.observation_space.n
 
     q_table = np.zeros((state_space_size, action_space_size))
