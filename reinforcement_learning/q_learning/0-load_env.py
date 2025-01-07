c87207d73bef3360934255009b651a47f8a1da66
meddizzle316 <meddizzle316@gmail.com>
2025-01-07 14:56:31 -0600

    trying 4

---
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""loads pre made gymnasium environment"""
+import gymnasium
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """loads pre made gymnasium environment"""
+    env = gymnasium.make("FrozenLake-v1", is_slippery=is_slippery, map_name=map_name)
+    return env
