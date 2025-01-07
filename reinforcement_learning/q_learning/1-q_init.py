009637554e552a9cb7a5d166125471f0e41574e0
meddizzle316 <meddizzle316@gmail.com>
2025-01-07 15:13:09 -0600

    first go at 1

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""initializes q table"""
+import numpy as np
+
+def q_init(env):
+    """initilizaes q table"""
+    action_space_size =  env.action_space.n
+    state_space_size = env.observation_space.n
+
+    q_table = np.zeros((state_space_size, action_space_size))
+    return q_table
