43dbf79c095d5af1be7018b89e8d164ce61a1155
meddizzle316 <meddizzle316@gmail.com>
2025-01-07 15:04:36 -0600

    trying 0

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -6,4 +6,6 @@ import gymnasium as gym
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """loads pre made gymnasium environment"""
     env = gym.make("FrozenLake-v1", is_slippery=is_slippery, map_name=map_name)
+    if map_name is None and is_slippery is None:
+        env = gym.make("FrozenLake-v1", is_slippery=is_slippery, map_name='8x8')
     return env
