155b623d613a37e003ae94caf5d440c1e819891c
meddizzle316 <meddizzle316@gmail.com>
2025-01-29 17:17:41 -0600

    trying task 2

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -49,3 +49,55 @@ def policy_gradient(state, weight):
     dlogpi = np.outer(state, np.eye(len(action_probs))[action] - action_probs)
 
     return action, dlogpi
+
+
+def calculate_rewards_andr(rewards, gamma):
+    """calculates discounted rewards"""
+    DiscountedReturns = []
+    for t in range(len(rewards)):
+        G = 0.0
+        for k, r in enumerate(rewards[t:]):
+            G += (gamma ** k) * r
+        DiscountedReturns.append(G)
+    return DiscountedReturns
+
+
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98, max_steps=500):
+    """trains a policy using monte carlo method"""
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+    theta = np.random.randn(state_dim, action_dim) * 0.01
+    total_rewards = []
+    episode_reward = 0.0
+
+    for episode in range(nb_episodes):
+        states, actions, rewards,  = [], [], []
+        episode_rewards, gradients = [], []
+        state, _ = env.reset()
+        for step in range(max_steps):
+            action, gradient = policy_gradient(state, theta)
+            next_state, reward, done, _, _ = env.step(action)
+
+            states.append(state)
+            actions.append(action)
+            rewards.append(reward)
+            gradients.append(gradient)
+
+            # episode_reward += reward * (gamma ** step)
+            state = next_state
+            if done:
+                break
+
+        discounted_rewards = calculate_rewards_andr(rewards, gamma)
+        # episode_rewards.append(episode_reward)
+
+        # for t, (loss_state, loss_action, G_t) in enumerate(zip(states,
+        # actions, discounted_rewards)):
+        for t in range(len(rewards)):
+            G_t = discounted_rewards[t]
+            theta += alpha * G_t * gradients[t]
+
+        print(f"Episode: {episode} Score: {sum(rewards)}")
+        total_rewards.append(sum(rewards))
+
+    return total_rewards
