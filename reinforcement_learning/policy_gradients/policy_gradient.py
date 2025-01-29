b11e35bb880d2e8b2351ba4804a425bc574a19c5
meddizzle316 <meddizzle316@gmail.com>
2025-01-29 17:12:55 -0600

    trying task 1

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -14,6 +14,33 @@ def policy(matrix, weight):
     apply softmax"""
     base = np.matmul(matrix, weight)
 
+    if base.ndim == 1:
+        base = base[np.newaxis, :]
+
     policy_softmax = softmax(base)
 
     return policy_softmax
+
+
+def policy_action(policy_softmax):
+    action_probabilites = policy_softmax.squeeze(0)  # since output of policy is 2d and we need 1d
+
+    num_possible_actions = len(action_probabilites)
+    # choose an action
+    action = np.random.choice(
+        np.arange(num_possible_actions), p=action_probabilites
+    )
+    return action, action_probabilites
+
+
+def policy_gradient(state, weight):
+    """computes Monte Carlo policy gradient, returns the action
+    and the log of the gradient"""
+    policy_softmax = policy(state,
+                            weight)  # applying softmax to make sure it's a valid probability distribution (positive and sums to 1)
+
+    action, action_probs = policy_action(policy_softmax)
+
+    dlogpi = np.outer(state, np.eye(len(action_probs))[action] - action_probs)
+
+    return action, dlogpi
