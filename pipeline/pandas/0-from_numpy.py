6e6ac680239afd5e05c49a483ef608baf66a3884
meddizzle316 <meddizzle316@gmail.com>
2025-02-17 15:28:23 -0600

    0 and 1

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """converts numpy array to dataframe"""
-import string
 
 import pandas as pd
 
@@ -9,7 +8,9 @@ def from_numpy(array):
     """converts pd df from numpy"""
 
     df = pd.DataFrame(array)
-    alphabet = string.ascii_uppercase
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I",
+                "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                "S", "T", "U", "V", "W", "X", "Y", "Z"]
     new_columns = {df.columns[i]: alphabet[i] for i in range(len(df.columns))}
     df = df.rename(columns=new_columns)
 
---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""creates df from dictionary"""
+import pandas as pd
+
+row_names= ["A", "B", "C", "D"]
+first_column = [0.0, 0.5, 1.0, 1.5]
+second_column = ["one", "two", "three", "four"]
+
+dict = {}
+dict['First'] = first_column
+dict['Second'] = second_column
+
+print(dict)
+
+df= pd.DataFrame(dict, index=row_names)
+
+print(df)
