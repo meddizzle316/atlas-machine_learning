ab37cbafb595197cfea868f8fb1b6dfc8f9f9738
meddizzle316 <meddizzle316@gmail.com>
2025-02-20 15:56:01 -0600

    11

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""concats a df"""
+import pandas as pd
+
+
+def concat(df1, df2):
+    """concats 2 dfs"""
+    df1.set_index("Timestamp",inplace=True)
+    df2.set_index("Timestamp", inplace=True)
+
+    target_index = df2.index.get_loc(1417411920)
+    df2_selected = df2[:target_index]
+
+    df_combine = pd.concat([df2_selected, df1], axis=0, keys=["bitstamp", "coinbase"]) # might be 1?
+
+    return df_combine
