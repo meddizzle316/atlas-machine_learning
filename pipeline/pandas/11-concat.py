942d752d7d9a14132fe65fe467f84ecb3fc839e4
meddizzle316 <meddizzle316@gmail.com>
2025-02-20 16:05:23 -0600

    11

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -6,12 +6,13 @@ index = __import__('10-index').index
 
 def concat(df1, df2):
     """concats 2 dfs"""
-    df1.set_index("Timestamp",inplace=True)
+    df1.set_index("Timestamp", inplace=True)
     df2.set_index("Timestamp", inplace=True)
 
     target_index = df2.index.get_loc(1417411920)
-    df2_selected = df2[:target_index+1]
+    df2_selected = df2[:target_index + 1]
 
-    df_combine = pd.concat([df2_selected, df1], axis=0, keys=["bitstamp", "coinbase"]) # might be 1?
+    df_combine = pd.concat([df2_selected, df1], axis=0, keys=[
+                           "bitstamp", "coinbase"])  # might be 1?
 
     return df_combine
