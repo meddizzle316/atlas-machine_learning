e8bb6c34a57c8a5655032b050de410ff51d732ec
meddizzle316 <meddizzle316@gmail.com>
2025-02-20 10:42:18 -0600

    9

---
+++ pipeline/pandas/9-fill.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""fills nan columns"""
+
+
+def fill(df):
+    df = df.drop("Weighted_Price", axis=1)
+    df["Close"] = df['Close'].fillna(method='ffill') # fills
+    df["High"].fillna(df["Close"], inplace=True)
+    df["Low"].fillna(df["Close"], inplace=True)
+    df["Open"].fillna(df["Close"], inplace=True)
+    df[["Volume_(BTC)", "Volume_(Currency)"]] = df[["Volume_(BTC)", "Volume_(Currency)"]].fillna(0)
+    return df
