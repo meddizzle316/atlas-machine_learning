676b88312e16f39ec8fbb4cfc9d5fe3849b1aab9
meddizzle316 <meddizzle316@gmail.com>
2025-02-24 14:47:48 -0600

    0

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""gets ships that have passenger count"""
+import requests
+
+
+def availableShips(passengerCount):
+    """returns ships that can hold passenger count"""
+    url = "https://swapi-api.hbtn.io/api/starships"
+    starships = requests.get(url)
+    starships_with_passengerCount = []
+
+    if starships.status_code == 200:
+        pass
+    else:
+        print(f"Error: {starships.status_code}")
+
+    try:
+        data = starships.json()
+    except ValueError:
+        print("Response is not in JSON format")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+    # print(json.dumps(data['results'], indent=4))
+
+    for ship in data['results']:
+        try:
+            passengers = int(ship['passengers'].replace(",", ""))
+            if passengers >= int(passengerCount):
+               starships_with_passengerCount.append(ship['name'])
+        except ValueError:
+            continue
+
+    return starships_with_passengerCount
