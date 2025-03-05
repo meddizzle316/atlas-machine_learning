1008fc15fcafc5fac5df266ba960966dd2a1ee73
meddizzle316 <meddizzle316@gmail.com>
2025-03-05 14:38:49 -0600

    13

--- pipeline/databases/13-uniq_users.sql
+++ pipeline/databases/13-uniq_users.sql
@@ -1,7 +1,7 @@
 -- a SQL script that creates a table users following these requirements:
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     PRIMARY KEY (id)
